c3e90c0346b3ca6e134fd91e3ddf72f3c25253d9
Gusradika <aditezo24@gmail.com>
2023-06-06 20:11:36 +0700

    Node Runtime

---
+++ L2-Runtime/coba.js
@@ -0,0 +1,7 @@
+console.log("Hello Coba");
+
+const cetakNama = (nama) => {
+  return `Hai, nama saya adalah ${nama}`;
+};
+
+module.exports = cetakNama; // export
---
+++ L2-Runtime/index.js
@@ -0,0 +1,5 @@
+// console.log("Hello Index");
+
+const cetakNama = require("./coba.js");
+
+console.log(cetakNama("Aditya"));
